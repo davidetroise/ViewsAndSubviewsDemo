d7269f04238b1cc325bb81224d29ed9e219aa0c2
Davide Troise <davide@troise.net>
2022-08-31 11:01:03 +0200

    Initial Commit

---
+++ ViewsAndSubviewsDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ViewsAndSubviewsDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewsAndSubviewsDemo/ViewsAndSubviewsDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ViewsAndSubviewsDemoApp.swift
+//  ViewsAndSubviewsDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndSubviewsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
